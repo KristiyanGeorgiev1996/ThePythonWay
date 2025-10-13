3c1203ed023dc621f17e9fbd1da9c55980089320
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-10-13 18:42:39 +0300

    Add files via upload

---
+++ 02.Programming-Fundamentals-Python/01.Tasks-Programming-Fundamentals-with-Python/03.Data-Types-and-Variables/11.RefactorVolumeOfPyramid.py
@@ -0,0 +1,7 @@
+length = float(input())
+width = float(input())
+height = float(input())
+
+volume = (length * width * height) / 3
+
+print(f"Length: {length}, Width: {width}, Height: {height}, Pyramid Volume: {volume:.2f}")
