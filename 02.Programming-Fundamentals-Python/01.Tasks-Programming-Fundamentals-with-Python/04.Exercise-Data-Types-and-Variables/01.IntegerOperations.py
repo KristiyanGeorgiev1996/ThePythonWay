03ca1196e92e741fac4da47854182ea0c0fb7145
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-10-13 18:43:14 +0300

    Add files via upload

---
+++ 02.Programming-Fundamentals-Python/01.Tasks-Programming-Fundamentals-with-Python/04.Exercise-Data-Types-and-Variables/01.IntegerOperations.py
@@ -0,0 +1,10 @@
+first = int(input())
+sec = int(input())
+third = int(input())
+fourth = int(input())
+
+smetka1 = first + sec
+smetka2 = smetka1 / third
+smetka3 = smetka2 * fourth
+
+print(smetka3)
