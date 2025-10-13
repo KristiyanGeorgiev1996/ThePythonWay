3c5c654430912b9333200a696af2890deb2ae5e3
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-10-13 18:39:39 +0300

    Add files via upload

---
+++ 02.Programming-Fundamentals-Python/01.Tasks-Programming-Fundamentals-with-Python/02.Exercise-Basic-Syntax-Conditional-Statements-and-Loops/13.EnglishNameOfTheLastDigit.py
@@ -0,0 +1,23 @@
+number = int(input())
+last_number = number % 10
+
+if last_number == 1:
+    print("one")
+elif last_number == 2:
+    print("two")
+elif last_number == 3:
+    print("three")
+elif last_number == 4:
+    print("four")
+elif last_number == 5:
+    print("five")
+elif last_number == 6:
+    print("six")
+elif last_number == 7:
+    print("seven")
+elif last_number == 8:
+    print("eight")
+elif last_number == 9:
+    print("nine")
+else:
+    print("zero")
