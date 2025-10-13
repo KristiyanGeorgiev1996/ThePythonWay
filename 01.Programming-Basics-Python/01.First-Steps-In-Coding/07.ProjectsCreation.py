0f2a448cff2b96902868db9ce8c462c3d5b40148
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-10-13 17:23:18 +0300

    Add files via upload

---
+++ 01.Programming-Basics-Python/01.First-Steps-In-Coding/07.ProjectsCreation.py
@@ -0,0 +1,5 @@
+name = input()
+numberOfProjects = int(input())
+
+neededHours = numberOfProjects * 3
+print(f"The architect {name} will need {neededHours} hours to complete {numberOfProjects} project/s.")
