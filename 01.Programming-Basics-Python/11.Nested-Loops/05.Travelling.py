69169fde7abf364d72d106a5e6d29a431b92f9a9
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-10-13 17:45:05 +0300

    Add files via upload

---
+++ 01.Programming-Basics-Python/11.Nested-Loops/05.Travelling.py
@@ -0,0 +1,12 @@
+destination = input()
+
+while destination != "End":
+    needed_money = float(input())
+    saved_money = 0
+
+    while saved_money < needed_money:
+        current_money = float(input())
+        saved_money += current_money
+
+    print(f"Going to {destination}!")
+    destination = input()
