4f3c3b5267bf8dedc14c2857874e2eef13095c31
Kristiyan Georgiev <kristiqn35@yahoo.com>
2025-10-14 17:29:00 +0300

    Add files via upload

---
+++ 02.Programming-Fundamentals-Python/01.Tasks-Programming-Fundamentals-with-Python/14.Exercise-Associative-Arrays/06.StudentAcademy.py
@@ -0,0 +1,15 @@
+n = int(input())
+students = {}
+
+for _ in range(n):
+    name = input()
+    grade = float(input())
+
+    if name not in students:
+        students[name] = []
+    students[name].append(grade)
+
+for name, grades in students.items():
+    average_grade = sum(grades) / len(grades)
+    if average_grade >= 4.5:
+        print(f"{name} -> {average_grade:.2f}")
